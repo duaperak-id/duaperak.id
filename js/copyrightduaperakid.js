7df02b51bda4b9e30538226ef4c0b67fe5bae1e0
anggasaputraqwe <masalikzxc@gmail.com>
2017-06-13 11:30:01 +0700

    Create copyrightduaperakid.js

---
+++ js/copyrightduaperakid.js
@@ -0,0 +1,9 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript" language="javascript"> $(function () { $(this).bind("contextmenu", function (e) { e.preventDefault(); }); }); </script>
+   <script>
+document.onkeydown = function(e) {
+        if (e.ctrlKey &&
+            (e.keyCode === 85 )) {
+            return false;
+        }
+};
