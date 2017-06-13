118f23f6b9114d94312fe854a5d481c6d5850ca4
anggasaputraqwe <masalikzxc@gmail.com>
2017-06-13 10:52:58 +0700

    Create redirec.js

---
+++ js/redirec.js
@@ -0,0 +1,21 @@
+<script type='text/javascript'>
+//<![CDATA[
+var blog = document.location.hostname;
+var slug = document.location.pathname;
+var ctld = blog.substr(blog.lastIndexOf("."));
+if (ctld != ".com") {
+var ncr = "https://" + blog.substr(0, blog.indexOf("."));
+ncr += ".blogspot.com/ncr" + slug;
+window.location.replace(ncr); };
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("https://www.duaperak.id?id=", "");
+var _0x38fc=["\x68\x69\x64\x65","\x2E\x56\x69\x73\x69\x74\x5F\x4C\x69\x6E\x6B","\x64\x65\x63\x6F\x64\x65","\x5F\x62\x6C\x61\x6E\x6B","\x6F\x70\x65\x6E","\x64\x6F\x77\x6E\x6C\x6F\x61\x64","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x64\x6F\x77\x6E\x6C\x6F\x61\x64\x32","\x62\x74\x6E","\x64\x61\x70\x6C\x6F\x6E\x67","\x68\x72\x65\x66","\x73\x70\x61\x6E","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x72\x65\x70\x6C\x61\x63\x65\x43\x68\x69\x6C\x64","\x70\x61\x72\x65\x6E\x74\x4E\x6F\x64\x65","\x64\x69\x73\x70\x6C\x61\x79","\x73\x74\x79\x6C\x65","\x6E\x6F\x6E\x65","\x69\x6E\x6C\x69\x6E\x65","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x3C\x68\x33\x20\x63\x6C\x61\x73\x73\x3D\x27\x74\x65\x78\x74\x2D\x69\x6E\x66\x6F\x27\x3E","\x20\x53\x65\x63\x6F\x6E\x64\x3C\x2F\x68\x33\x3E","\x6F\x6E\x6C\x6F\x61\x64","\x6D\x61\x69\x6E\x74\x65\x6E\x63\x65","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x61\x6E\x74\x6F\x6E\x63\x61\x62\x6F\x6E\x2E\x75\x73\x2F","\x73\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65","\x74\x69\x74\x6C\x65","\x20\x41\x6E\x74\x6F\x6E\x63\x61\x62\x6F\x6E","\x41\x6E\x74\x6F\x6E\x63\x61\x62\x6F\x6E"];$(_0x38fc[1])[_0x38fc[0]]();function changeLink(){var _0xc21ax2=Base64[_0x38fc[2]](res);window[_0x38fc[4]](_0xc21ax2,_0x38fc[3])}function generate(){var _0xc21ax4=document[_0x38fc[6]](_0x38fc[5]),_0xc21ax5=document[_0x38fc[6]](_0x38fc[7]),_0xc21ax6=document[_0x38fc[6]](_0x38fc[8]),_0xc21ax7=document[_0x38fc[6]](_0x38fc[9]),_0xc21ax8=document[_0x38fc[6]](_0x38fc[5])[_0x38fc[10]],_0xc21ax9=9;var _0xc21axa=document[_0x38fc[12]](_0x38fc[11]);_0xc21ax4[_0x38fc[14]][_0x38fc[13]](_0xc21axa,_0xc21ax4);var _0xc21axb;_0xc21axb= setInterval(function(){_0xc21ax9--;if(_0xc21ax9< 0){_0xc21axa[_0x38fc[14]][_0x38fc[13]](_0xc21ax4,_0xc21axa);clearInterval(_0xc21axb);_0xc21ax7[_0x38fc[16]][_0x38fc[15]]= _0x38fc[17];_0xc21ax4[_0x38fc[16]][_0x38fc[15]]= _0x38fc[18];_0xc21ax5[_0x38fc[16]][_0x38fc[15]]= _0x38fc[18]}else {_0xc21axa[_0x38fc[19]]= _0x38fc[20]+ _0xc21ax9.toString()+ _0x38fc[21];_0xc21ax6[_0x38fc[16]][_0x38fc[15]]= _0x38fc[17]}},2000)}window[_0x38fc[22]]= function(){var _0xc21axc=document[_0x38fc[6]](_0x38fc[23]);if(_0xc21axc== null){window[_0x38fc[24]][_0x38fc[10]]= _0x38fc[25]};_0xc21axc[_0x38fc[26]](_0x38fc[10],_0x38fc[25]);_0xc21axc[_0x38fc[26]](_0x38fc[27],_0x38fc[28]);_0xc21axc[_0x38fc[19]]= _0x38fc[29]}
+
+cookieOptions = {
+    msg: "This site uses cookies to help deliver services. By using this site, you agree to the use of cookies.",
+    link: "https://www.blogger.com/go/blogspot-cookies",
+    close: "Got it!",
+    learn: "Learn More" };
+//]]>
+</script>
