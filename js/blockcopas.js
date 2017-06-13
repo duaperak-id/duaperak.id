93058725c4d4b6dbe6fe8d47a282e6e7f090b46d
anggasaputraqwe <masalikzxc@gmail.com>
2017-06-13 11:13:21 +0700

    Update blockcopas.js

--- js/blockcopas.js
+++ js/blockcopas.js
@@ -1,6 +1,6 @@
 function seeU(e){
 if (e.ctrlKey && e.which == 85){
-window.location.replace('https://www.duaperak.id/');
+window.location.replace('https://www.duaperak.id/p/kenadeh.html');
 return false;}}
 document.onkeydown = seeU;
 //]]>
